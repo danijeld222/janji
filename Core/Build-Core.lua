4bd6891b61517972f99be5a43c922d250994b144
danijeld222 <danijel742.dd@gmail.com>
2024-04-18 01:09:26 +0200

    Dani - added SDL3 SDL3_Image SDL3_Mixer SDL3_TTF libs

--- Core/Build-Core.lua
+++ Core/Build-Core.lua
@@ -11,6 +11,22 @@ project "Core"
    {
       "Source"
    }
+   
+   externalincludedirs
+   {
+      "Vendor/SDL3/include",
+      "Vendor/SDL3_Image/include",
+      "Vendor/SDL3_Mixer/include",
+      "Vendor/SDL3_TTF/include"
+   }
+   
+   links
+   {
+      "SDL3",
+      "SDL3_Image",
+      "SDL3_Mixer",
+      "SDL3_TTF"
+   }
 
    targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
    objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
@@ -18,10 +34,25 @@ project "Core"
    filter "system:windows"
        systemversion "latest"
        defines { "COREEXPORT" }
+       libdirs
+       {
+          "Vendor/SDL3/lib/%{cfg.architecture}",
+          "Vendor/SDL3_Image/lib/%{cfg.architecture}",
+          "Vendor/SDL3_Mixer/lib/%{cfg.architecture}",
+          "Vendor/SDL3_TTF/lib/%{cfg.architecture}"
+       }
        postbuildcommands
        {
           "{COPY} ../Binaries/" .. OutputDir .. "/%{prj.name}/*.dll ../Binaries/" .. OutputDir .. "/App",
-          "{COPY} ../Binaries/" .. OutputDir .. "/%{prj.name}/*.dll ../Binaries/" .. OutputDir .. "/AppTest"
+          "{COPY} ../Binaries/" .. OutputDir .. "/%{prj.name}/*.dll ../Binaries/" .. OutputDir .. "/AppTest",
+          "{COPY} $(SolutionDir)%{prj.name}/Vendor/SDL3/lib/%{cfg.architecture}/SDL3.dll ../Binaries/" .. OutputDir .. "/App",
+          "{COPY} $(SolutionDir)%{prj.name}/Vendor/SDL3/lib/%{cfg.architecture}/SDL3.dll ../Binaries/" .. OutputDir .. "/AppTest",
+          "{COPY} $(SolutionDir)%{prj.name}/Vendor/SDL3_Image/lib/%{cfg.architecture}/SDL3_image.dll ../Binaries/" .. OutputDir .. "/App",
+          "{COPY} $(SolutionDir)%{prj.name}/Vendor/SDL3_Image/lib/%{cfg.architecture}/SDL3_image.dll ../Binaries/" .. OutputDir .. "/AppTest",
+          "{COPY} $(SolutionDir)%{prj.name}/Vendor/SDL3_Mixer/lib/%{cfg.architecture}/SDL3_mixer.dll ../Binaries/" .. OutputDir .. "/App",
+          "{COPY} $(SolutionDir)%{prj.name}/Vendor/SDL3_Mixer/lib/%{cfg.architecture}/SDL3_mixer.dll ../Binaries/" .. OutputDir .. "/AppTest",
+          "{COPY} $(SolutionDir)%{prj.name}/Vendor/SDL3_TTF/lib/%{cfg.architecture}/SDL3_ttf.dll ../Binaries/" .. OutputDir .. "/App",
+          "{COPY} $(SolutionDir)%{prj.name}/Vendor/SDL3_TTF/lib/%{cfg.architecture}/SDL3_ttf.dll ../Binaries/" .. OutputDir .. "/AppTest"
        }
 
    filter "configurations:Debug"
